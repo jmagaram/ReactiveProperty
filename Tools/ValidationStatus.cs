f3b5ad876b1620904a51d9ff00ca0b58caf88252
justi <justi@SURFACEPRO4-JUS>
2016-07-11 07:39:44 -0700

    remove None

--- Tools/ValidationStatus.cs
+++ Tools/ValidationStatus.cs
@@ -4,53 +4,41 @@ namespace Tools {
     [Flags]
     public enum ValidationStatus {
         /// <summary>
-        /// Cross-property validation can not start or is irrelevant until validation rules on
-        /// dependent properties are satisfied.
-        /// </summary>
-        /// <example>
-        /// Street, city, and zip are necessary components of a valid address. If any of these
-        /// properties are missing or formatted incorrectly, there is no point in attempting to
-        /// validate the address as a whole (perhaps by using an internet mapping service). In such a
-        /// situation, the cross-property validation status is <see cref="ValidationStatus.Blocked"/>.
-        ///
-        /// The status of an address with invalid components could optionally be reported as <see
-        /// cref="ValidationStatus.HasErrors"/>. But this might draw the user's attention to a
-        /// message like "Could not find this address using the Google mapping service." It is better
-        /// focus the user on root-cause errors they can fix. A status of <see
-        /// cref="ValidationStatus.Blocked"/> could be used to display a message like "The address
-        /// could not be validated; fix the problems noted below."
-        ///
-        /// The cross-property validation status of the address could also be reported as <see
-        /// cref="ValidationStatus.IsValid"/>. This will not not erase the errors on the individual
-        /// components but it just feels wrong. Information is lost that could be useful for display
-        /// error status in the UI.
-        /// </example>
-        Blocked = 1,
-
-        /// <summary>
         /// Asynchronous validation is in progress.
         /// </summary>
-        Running = 2,
+        Running,
 
         /// <summary>
-        /// Asynchronous validation was canceled. 
+        /// Asynchronous validation was canceled (e.g. the user grew tired of waiting for a lengthy
+        /// web request to complete).
         /// </summary>
-        Canceled = 4,
+        Canceled,
 
         /// <summary>
         /// Validation ended unexpectedly (e.g. network is down). 
         /// </summary>
-        Faulted = 8,
+        Faulted,
+
+        /// <summary>
+        /// Cross-property validation can not start or is irrelevant until validation rules on
+        /// dependent properties are satisfied.
+        /// </summary>
+        /// <example>
+        /// Street, city, and zip are components of a valid address. If any of these properties are
+        /// missing or formatted incorrectly, there is no point in attempting to validate the address
+        /// as a whole (perhaps by using an internet mapping service).
+        /// </example>
+        Blocked,
 
         /// <summary>
         /// Validation completed. One or more validation rules were not satisfied.
         /// </summary>
-        HasErrors = 16,
-        
+        HasErrors,
+
         /// <summary>
         /// Validation completed. All validation rules were satisfied.
         /// </summary>
-        IsValid = 32,
+        IsValid,
 
         /// <summary>
         /// It is unknown if all validation rules are satisfied; the validation process is running or did not complete normally.
